d92c119423f5d18ba0914bb29179c257fd75a2f3
Brian Trager <brian.p.trager@outlook.com>
2020-04-06 12:58:50 -0400

    Initial commit

---
+++ DataBinding/DataBinding/DataBinding/Book.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DataBinding
+{
+    class Book : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        string author;
+        public string Author
+        {
+            get { return author; }
+            set
+            {
+                if (value != author)
+                {
+                    author = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Author)));
+                }
+            }
+        }
+        
+        private string title;
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+        
+        private int year;
+        public int Year
+        {
+            get { return year; }
+            set { year = value; }
+        }
+        
+        private string isbn;
+        public string ISBN
+        {
+            get { return isbn; }
+            set { isbn = value; }
+        }
+
+    }
+}
---
+++ DataBinding/DataBinding/DataBinding/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DataBinding
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            Book book = new Book() { Author = "John Grisham", Title = "A Time To Kill", Year = 1992, ISBN = "978-1033043294" };
+            InitializeComponent();
+            BindingContext = book;
+
+            //Binding authorBinding = new Binding();
+            //authorBinding.Source = book;
+            //authorBinding.Path = "Author";
+            //Author.SetBinding(Entry.TextProperty, authorBinding);
+
+            //Binding titleBinding = new Binding();
+            //titleBinding.Source = book;
+            //titleBinding.Path = "Title";
+            //Title.SetBinding(Entry.TextProperty, titleBinding);
+
+            //Author.Text = book.Author;
+            //Title.Text = book.Title;
+            //Year.Text = book.Year.ToString();
+            //ISBN.Text = book.ISBN;
+        }
+    }
+}
